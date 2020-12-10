a7dc51836034e1a943814ed96678671295ba4bdd
Lera Chukovich <chukalera@gmail.com>
2020-12-10 15:05:40 +0100

    solve string task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -206,12 +206,11 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  // const top = '┌' + '─'.repeat(width - 2) + '┐\n';
-  // const middle = ('│' + ' '.repeat(width - 2) + '│\n').repeat(height - 2);
-  // const bottom = '└' + '─'.repeat(width - 2) + '┘';
-  // return top + middle + bottom;
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  const top = `┌${'─'.repeat(width - 2)}┐\n`;
+  const middle = (`│${' '.repeat(width - 2)}│\n`).repeat(height - 2);
+  const bottom = `└${'─'.repeat(width - 2)}┘\n`;
+  return top + middle + bottom;
 }
 
 
@@ -231,8 +230,8 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  return str.replace(/[a-zA-Z]/g, (s) => String.fromCharCode(s.charCodeAt(0) + (s.toUpperCase() <= 'M' ? 13 : -13)));
 }
 
 /**
@@ -248,8 +247,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 
